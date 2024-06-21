b0c786668b0ea5431763f74e5833ab38960cdd84
Ishita Ahelleya <ishita.ahelleya825@gmail.com>
2024-06-20 20:31:20 -0700

    Initial Commit

---
+++ Shared/AlzAssistantApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlzAssistantApp.swift
+//  Shared
+//
+//  Created by Ishita App on 6/20/24.
+//
+
+import SwiftUI
+
+@main
+struct AlzAssistantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
